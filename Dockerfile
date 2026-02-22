ff10089de91abe6b5d04a5e2a302167837f8e7d3
Shadowmataj <christianmataj1@gmail.com>
2026-02-22 03:39:43 -0600

    Fix env_file in docker compose, add caffeine, fix logs, fix hibernet open-in-view

--- Dockerfile
+++ Dockerfile
@@ -22,7 +22,7 @@ WORKDIR /app
 COPY --from=build /app/target/*.jar app.jar
 
 # Expose the application port
-EXPOSE 8765
+EXPOSE 8090
 
 # Set the entry point
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/portability/bot_service/config/AppConfig.java
+++ src/main/java/com/portability/bot_service/config/AppConfig.java
@@ -2,12 +2,14 @@ package com.portability.bot_service.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.portability.bot_service.service.PostgresChatMemory;
 
 @Configuration
+@EnableCaching
 public class AppConfig {
 
     @Bean
